ee8a1c875b376f8988ae7fff94e475d5a0a13bcd
peareareater <melonistasty@gmail.com>
2020-09-06 23:11:24 +0300

    Updates

---
+++ precache-manifest.72ba267b456ab95f227e0f890132f888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f94877b1083cbf7c8851198b570184",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "2f112d6b99088a8c1ec3",
+    "url": "/test-app/static/css/main.ea093327.chunk.css"
+  },
+  {
+    "revision": "d1795b59121c8250e6ad",
+    "url": "/test-app/static/js/2.e9b46f95.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/test-app/static/js/2.e9b46f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f112d6b99088a8c1ec3",
+    "url": "/test-app/static/js/main.9872085c.chunk.js"
+  },
+  {
+    "revision": "010976abd684515c15d4",
+    "url": "/test-app/static/js/runtime-main.107bf183.js"
+  }
+]);
